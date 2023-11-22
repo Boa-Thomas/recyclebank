1d45f86ca79256292775ff0b2b7d97c8e5b95888
Thomas Topfstedt <thotop100@gmail.com>
2023-11-22 13:48:51 -0300

    fix

--- Atuacao_Reles.py
+++ Atuacao_Reles.py
@@ -9,17 +9,21 @@ def setup_relays():
     for pin in relay_pins:
         GPIO.setup(pin, GPIO.OUT)
 
-def activate_relay1_and_relay3():
-    GPIO.output(2, True)  # Ativar IN1
-    GPIO.output(4, True)  # Ativar IN3
+def activate_relay1_and_relay4():
     GPIO.output(3, False) # Desativar IN2
-    GPIO.output(17, False) # Desativar IN4
-
-def activate_relay2_and_relay4():
-    GPIO.output(2, False) # Desativar IN1
     GPIO.output(4, False) # Desativar IN3
+    time.sleep(0.1)      # Delay de 100ms
+    GPIO.output(2, True)  # Ativar IN1
+    GPIO.output(17, True) # Ativar IN4
+
+
+
+def activate_relay2_and_relay3():
+    GPIO.output(2, False) # Desativar IN1
+    GPIO.output(17, False) # Desativar IN4
+    time.sleep(0.1)       # Delay de 100ms
     GPIO.output(3, True)  # Ativar IN2
-    GPIO.output(17, True)  # Ativar IN4
+    GPIO.output(4, True)  # Ativar IN3
 
 def cleanup():
     GPIO.cleanup()
@@ -27,9 +31,9 @@ def cleanup():
 # Exemplo de uso:
 try:
     setup_relays()
-    activate_relay1_and_relay3()  # Ativar IN1 e IN3
-    time.sleep(20)  # Manter ativo por 5 segundos
-    activate_relay2_and_relay4()  # Ativar IN2 e IN4
-    time.sleep(20)  # Manter ativo por 5 segundos
+    activate_relay1_and_relay4()  # Ativar IN1 e IN4
+    time.sleep(20)               # Manter ativo por 20 segundos
+    activate_relay2_and_relay3()  # Ativar IN2 e IN3
+    time.sleep(20)               # Manter ativo por 20 segundos
 except KeyboardInterrupt:
     cleanup()
