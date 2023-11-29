2680f3635f5aa6998d669d967e58a5353036dcaa
Thomas Topfstedt <thotop100@gmail.com>
2023-11-29 08:55:33 -0300

    Update Atuacao_Reles.py

--- Atuacao_Reles.py
+++ Atuacao_Reles.py
@@ -16,6 +16,11 @@ def activate_relay1_and_relay4():
     GPIO.output(2, True)  # Ativar IN1
     GPIO.output(17, True) # Ativar IN4
 
+def all_off():
+    GPIO.output(2, False) # Desativar IN1
+    GPIO.output(3, False) # Desativar IN2
+    GPIO.output(4, False) # Desativar IN3
+    GPIO.output(17, False) # Desativar IN4
 
 
 def activate_relay2_and_relay3():
@@ -30,10 +35,12 @@ def cleanup():
 
 # Exemplo de uso:
 try:
+ setup_relays()
  while 1:
-    setup_relays()
+    all_off()
     activate_relay1_and_relay4()  # Ativar IN1 e IN4
     time.sleep(2)               # Manter ativo por 20 segundos
+    all_off()
     activate_relay2_and_relay3()  # Ativar IN2 e IN3
     time.sleep(2)               # Manter ativo por 20 segundos
 except KeyboardInterrupt:
