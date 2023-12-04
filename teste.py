d7012a6da512135df245dd8f00c385434f871f21
Thomas Topfstedt <thotop100@gmail.com>
2023-12-04 20:28:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,13 @@
 import serial
+import requests
+import time
 
-# Replace '/dev/ttyACM0' with the serial port that your Arduino is connected to.
-# The baud rate should be set to the same rate you've set in your Arduino program (usually in Serial.begin()).
+# Replace '/dev/ttyUSB0' with your Arduino's serial port.
 serial_port = '/dev/ttyUSB0'
 baud_rate = 9600
+
+# URL of the web server endpoint where you want to send the data.
+post_url = 'http://yourserver.com/post-endpoint'
 
 # Set up the serial connection
 ser = serial.Serial(serial_port, baud_rate, timeout=1)
@@ -25,3 +29,14 @@ while True:
             # Parse the current value and store it in the variable
             input_current = float(line.split('=')[1].strip())
             print(f"Stored current value: {input_current} A")
+
+        # Send a POST request to the server with the voltage and current values
+        if input_voltage is not None and input_current is not None:
+            data = {'voltage': input_voltage, 'current': input_current}
+            response = requests.post(post_url, data=data)
+            print(f"Response from server: {response.text}")
+
+            # Reset the variables and add a delay to limit the number of requests
+            input_voltage = None
+            input_current = None
+            time.sleep(10)  # Adjust the delay as needed
